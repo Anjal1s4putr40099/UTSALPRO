bccbbdb73eb5a8206552db7c0b8ce5d6d6ad4dd8
Anjal1s4putr40099 <118984588+Anjal1s4putr40099@users.noreply.github.com>
2022-11-24 20:15:33 +0700

    Create variabel.java

---
+++ variabel.java
@@ -0,0 +1,11 @@
+public class variabel{
+    public static void main(String[] args)  {
+        String salam = "Assalamualikum";
+        String nama = "Anjali Saputra";
+        int lahir = 2004;
+        
+        System.out.println(salam);
+        System.out.println("nama Saya "+nama);
+        System.out.println("saya Lahir di tahun : "+lahir);
+    }
+}
